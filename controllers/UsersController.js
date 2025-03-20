fa50cc775a9ad8ec7ffa84464fd66cd6aa70683f
AllenKeelig <allen.keeling@atlasschool.com>
2025-03-19 22:10:48 -0500

    4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,17 +1,16 @@
 import dbClient from '../utils/db.js';
 import { ObjectId } from 'mongodb';
 import crypto from 'crypto';
+import redisClient from '../utils/redis.js';
 
 class UsersController {
     static async postNew(req, res) {
         const { email, password } = req.body;
 
-        // Check if email is missing
         if (!email) {
             return res.status(400).json({ error: 'Missing email' });
         }
 
-        // Check if password is missing
         if (!password) {
             return res.status(400).json({ error: 'Missing password' });
         }
@@ -30,9 +29,29 @@ class UsersController {
             password: hashedPassword,
         });
 
-        // Respond with the new user info (excluding the password)
         return res.status(201).json({ id: newUser.insertedId, email });
     }
-}
+
+    static async getMe(req, res) {
+        const token = req.header('X-Token');
+        if (!token) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        const userId = await redisClient.get(`auth_${token}`);
+        if (!userId) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        const user = await dbClient.db.collection('users').findOne({ _id: new ObjectId(userId) });
+        if (!user) {
+          return res.status(401).json({ error: 'Unauthorized' });
+        }
+    
+        return res.status(200).json({ id: user._id, email: user.email });
+      }
+    }
+    
+
 
 export default UsersController;
